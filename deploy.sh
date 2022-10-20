c38bf40de92f334becbd6bc7c07b782d2c18a12f
morita <morichika.816@gmail.com>
2022-10-21 00:13:23 +0900

    first commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# first 
+git init
+git remote add origin https://github.com/botaidemy-cm/flask-app.git
+git branch -m main
+
+# deploy
+git add .
+git commit -m "first commit"
+git push -f origin main  
+
